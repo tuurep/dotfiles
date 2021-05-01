ba8855d4b10e2ba01b7ff7db4ff151cfd853c0cb
Tuure <tuure.piitulainen@gmail.com>
2021-05-01 11:48:25 +0300

    Remove redundancies

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -1,12 +1,9 @@
 from IPython.terminal.prompts import Token
 
-ipy_config = get_config()
-
-fg = '#c5c8c6'
 green = '#b5bd68'
 red = '#cc6666'
 
-ipy_config.TerminalInteractiveShell.highlighting_style_overrides = {
+c.TerminalInteractiveShell.highlighting_style_overrides = {
     Token.Prompt: green,
     Token.PromptNum: green,
     Token.OutPrompt: red,
