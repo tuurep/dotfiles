e46d57d604aaf276877e9e258def544bc5a79da6
Tuure <tuure.piitulainen@gmail.com>
2021-09-12 16:35:35 +0300

    Add volume script and update keybindings

---
+++ scripts/ob/increase-volume-with-upper-limit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sink=0
+interval=$1
+
+# Graphical volume sliders cap at 153%, follow suite here
+upper_limit=153
+
+current_volume=$(pactl get-sink-volume $sink | grep -Po '\d+(?=%)' | head -1)
+
+if [ $(($current_volume + $interval)) -le $upper_limit ]
+then
+    pactl -- set-sink-volume $sink +$interval%
+else
+    pactl -- set-sink-volume $sink $upper_limit%
+fi
