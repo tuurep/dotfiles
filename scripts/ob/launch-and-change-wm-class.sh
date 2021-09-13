ce94211160d9ab340eef862e274d09c783f2aaf5
Tuure <tuure.piitulainen@gmail.com>
2021-09-13 05:35:36 +0300

    Add desktop entry and launch script

---
+++ scripts/ob/launch-and-change-wm-class.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# A terrible hack (?) to use in .desktop files to set custom WM_CLASS
+# For applications where customizing WM_CLASS is not simple
+
+if [[ $# -ne 3 ]]
+then
+    echo "Usage: launch-and-change-wm-class <application> <old_WM_CLASS> <new_WM_CLASS>"
+    exit 1
+fi
+
+$1 &
+xdotool search --sync --class $2 \
+    set_window --class $3 %@
