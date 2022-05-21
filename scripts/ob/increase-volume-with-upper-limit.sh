0d77446586aa30227e4113195df0629436a488d5
Tuure <tuure.piitulainen@gmail.com>
2022-05-21 15:33:20 +0300

    Adjust volume and brightness binds and scripts

--- scripts/ob/increase-volume-with-upper-limit.sh
+++ scripts/ob/increase-volume-with-upper-limit.sh
@@ -3,8 +3,9 @@
 sink=0
 interval=$1
 
-# Graphical volume sliders cap at 153%, follow suite here
-upper_limit=153
+# Pavucontrol graphical slider caps at 153 (%)
+# Cap at 150 here (a nice round number)
+upper_limit=150
 
 current_volume=$(pactl get-sink-volume $sink | grep -Po '\d+(?=%)' | head -1)
 
--- scripts/ob/set-brightness.sh
+++ scripts/ob/set-brightness.sh
@@ -4,6 +4,14 @@ value=$1
 
 # Get current brightness as percentage with -P flag
 current_brightness=$(brightnessctl -P g)
+
+# If at 1% brightness, treat it as if being at 0 when doing a large increment
+# so that it doesn't "feel" like new value is off by 1
+if [[ $current_brightness == 1 && $value -gt 1 ]]
+then
+        value=$((value - 1))
+fi
+
 new_brightness=$((current_brightness + value))
 
 # Don't allow going to 0 because screen will be literally black
