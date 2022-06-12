44614e186b8a81ce21266433e266b629f25ad27a
Tuure <tuure.piitulainen@gmail.com>
2022-06-12 15:47:29 +0300

    Decrease key repeat delay and remove unneeded &

--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -4,10 +4,10 @@
 hsetroot -solid '#000000'
 
 # Map capslock to esc
-setxkbmap -option caps:escape &
+setxkbmap -option caps:escape
 
 # Key repeat: ms, interval
-xset r rate 400 25 &
+xset r rate 305 25
 
 sh ~/.config/polybar/launch.sh &
 
