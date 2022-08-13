273d85aca3f9ea78ce86cdbbf6c9b19995a54e1b
Tuure <tuure.piitulainen@gmail.com>
2022-08-14 02:45:54 +0300

    Fix brightness controls for new laptop
    With new controller going to 0% does not result in a black screen,
    simplifies set-brightness script significantly.
    
    Note: performance issue when holding down F5/F6, which worked snappy
    in previous laptop with far worse specs. Why's that?

--- scripts/ob/set-brightness.sh
+++ scripts/ob/set-brightness.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
 
+# This script is meant so that Polybar shows the
+# brightness percentage in neat increments from 0-100
+# when the raw value is 0-255 and using direct
+# percentage increments/decrements with brightnessctl s
+# will result in rounding errors without floating
+# point arithmetic.
+
 value=$1
 
 # Get current brightness as percentage with -P flag
 current_brightness=$(brightnessctl -P g)
 
-# If at 1% brightness, treat it as if being at 0 when doing a large increment
-# so that it doesn't "feel" like new value is off by 1
-if [[ $current_brightness == 1 && $value -gt 1 ]]
-then
-        value=$((value - 1))
-fi
-
-new_brightness=$((current_brightness + value))
-
-# Don't allow going to 0 because screen will be literally black
-# Note: would be possible to set minimum value within brightnessctl maybe
-# but hey, if it works, it works!
-if [ $new_brightness -gt 0 ]
-then
-        brightnessctl s "$new_brightness"%
-else
-        brightnessctl s 1%
-fi
+brightnessctl s $(($current_brightness + $value))%
