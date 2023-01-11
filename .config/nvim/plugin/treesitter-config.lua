98722a6c692a526a24844c02c4d336743b85da06
Tuure <tuure.piitulainen@gmail.com>
2023-01-11 11:02:47 +0200

    Disable indent module in nvim-treesitter

--- .config/nvim/plugin/treesitter-config.lua
+++ .config/nvim/plugin/treesitter-config.lua
@@ -2,11 +2,15 @@ require'nvim-treesitter.configs'.setup {
     ensure_installed = "all",
     sync_install = false,
     auto_install = false,
-    ignore_install = { "" },
+    ignore_install = { "comment" },
 
     highlight = {
         enable = true,
         disable = { "" },
-        additional_vim_regex_highlighting = false,
+        additional_vim_regex_highlighting = false
+    },
+
+    indent = {
+        enable = false
     }
 }
