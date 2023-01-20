1b3cf60943a1b3943c2e7ecaf01af770aa0b15a3
Tuure <tuure.piitulainen@gmail.com>
2023-01-20 15:21:12 +0200

    Fix (?) python erratic indentation with treesitter
    Had issues with tab width all over the place especially when inserting
    code in some nested structures and stuff. This issue is the most
    relevant:
    
    https://github.com/nvim-treesitter/nvim-treesitter/issues/3793
    
    Turns out it works when `set syntax` is on, which can be configured per
    language in nvim-treesitter config

--- .config/nvim/plugin/treesitter-config.lua
+++ .config/nvim/plugin/treesitter-config.lua
@@ -7,7 +7,7 @@ require'nvim-treesitter.configs'.setup {
     highlight = {
         enable = true,
         disable = { "latex" },
-        additional_vim_regex_highlighting = false
+        additional_vim_regex_highlighting = { "python" }
     },
 
     indent = {
