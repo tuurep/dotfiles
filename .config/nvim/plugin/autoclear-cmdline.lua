b25438f284cee8213bb8ac3631878f5aa95f809a
Tuure <tuure.piitulainen@gmail.com>
2023-08-23 03:00:17 +0300

    Fix overlapping timers
    Previously a message could be hidden too early if another timer was
    already running.
    
    Remaining problem: there are many kinds of messages I'd like to
    autohide, but which are unrelated to the event "CmdLineLeave"
    
    If only there was an event something like "MessageShown"

--- .config/nvim/plugin/autoclear-cmdline.lua
+++ .config/nvim/plugin/autoclear-cmdline.lua
@@ -1,8 +1,21 @@
-local function clear() vim.cmd("echo ''") end
+-- TODO:
+-- This only clears messages that are shown after doing something on command mode
+--
+-- How to clear any message, for example
+--      y     - n lines yanked
+--      u     - n changes; this and that
+--      <C-r> - Already at newest change
+--
+-- ?
+
+local timer = vim.loop.new_timer()
+local timeout_sec = 15
+
+local function clear_cmdline() vim.cmd("echo ''") end
 
 vim.api.nvim_create_autocmd("CmdLineLeave", {
     callback = function()
-        local seconds = 15
-        vim.fn.timer_start(1000*seconds, clear)
+        timer:stop()
+        timer:start(1000*timeout_sec, 0, vim.schedule_wrap(clear_cmdline))
     end
 })
