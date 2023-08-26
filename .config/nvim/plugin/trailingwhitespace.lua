df7e5b3b566339dd8707feea008c7d68bc9ac3a2
Tuure <tuure.piitulainen@gmail.com>
2023-08-26 12:43:03 +0300

    Fix trailing whitespace script for multiple splits

--- .config/nvim/plugin/trailingwhitespace.lua
+++ .config/nvim/plugin/trailingwhitespace.lua
@@ -5,18 +5,41 @@ local red = 1
 
 vim.api.nvim_set_hl(0, "TrailingWhitespace", { ctermfg=fg, ctermbg=red })
 
-local toggle_key = "ä"
-local shown = nil
+local matches = {}
+local shown = false
 
-local function toggle()
-    if not shown then
-        print("Trailing whitespace SHOWN")
-        shown = vim.fn.matchadd("TrailingWhitespace", "\\s\\+$")
-    else
-        print("Trailing whitespace HIDDEN")
-        vim.fn.matchdelete(shown)
-        shown = nil
+local function enable_hl(window)
+    if not matches[window] then
+        matches[window] = vim.fn.matchadd("TrailingWhitespace", "\\s\\+$")
     end
 end
 
-vim.keymap.set('n', toggle_key, toggle)
+local function toggle()
+    local wlist = vim.api.nvim_tabpage_list_wins(0)
+
+    for _, w in ipairs(wlist) do
+        vim.api.nvim_win_call(w, function()
+            if not shown then
+                enable_hl(w)
+            else
+                vim.fn.matchdelete(matches[w])
+                matches[w] = nil
+            end
+        end)
+    end
+
+    shown = not shown
+    print("Trailing whitespace " .. (shown and "SHOWN" or "HIDDEN"))
+end
+
+vim.api.nvim_create_autocmd({"WinEnter"}, {
+    callback = function()
+        local new_win = vim.api.nvim_get_current_win()
+        if shown then
+            enable_hl(new_win)
+        end
+    end
+})
+
+local toggle_key = "ä"
+vim.keymap.set("n", toggle_key, toggle)
