67d38e602a8e3936f00bceaf0348c0331565b186
Tuure <tuure.piitulainen@gmail.com>
2023-09-11 04:37:12 +0300

    Add nvim minimal config for troubleshooting

---
+++ .config/nvim/test/minrc.lua
@@ -0,0 +1,22 @@
+-- vim.g.foo = true
+
+
+--
+
+for _, plugin in ipairs({
+    -- "foo-author/bar-plugin",
+
+
+    --
+}) do
+    local install_path = vim.fn.fnamemodify("plugins" .. plugin:match("/%S+$"), ":p")
+    if vim.fn.isdirectory(install_path) == 0 then
+        vim.fn.system {
+            "git", "clone", "--depth=1",
+            "https://github.com/" .. plugin, install_path
+        }
+    end
+    vim.opt.runtimepath:append(install_path)
+end
+
+-- nvim --clean -u ~/.config/nvim/test/minrc.lua
