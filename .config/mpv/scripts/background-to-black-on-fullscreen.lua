befb9190a4b09496764d1242985056df36b8a6b3
Tuure <tuure.piitulainen@gmail.com>
2024-02-12 01:21:13 +0200

    Add mpv script: different bg color on fullscreen
    Works perfectly!

---
+++ .config/mpv/scripts/background-to-black-on-fullscreen.lua
@@ -0,0 +1,9 @@
+function on_fullscreen_change(name, value)
+    if value == true then
+        mp.set_property("background", "#000000")
+    else
+        mp.set_property("background", "#0f0f0f")
+    end
+end
+
+mp.observe_property("fullscreen", "bool", on_fullscreen_change)
