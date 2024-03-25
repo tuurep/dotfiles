a94fa04579ccb50d533f4b1131b14a0acb737558
Tuure <tuure.piitulainen@gmail.com>
2024-03-25 17:44:10 +0200

    Fix light-locker unwantedly locking after idle time
    `--lock-after-screensaver=0` is the one that matters in this case, but
    lets just give it all because the idea is that I'll only lock the screen
    manually on a keybind. Ain't nobody got time for lock screens when I
    didn't mean to lock it.

--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Set desktop 'wallpaper' as solid black color
 # Note: xsetroot -solid is incompatible with picom
 # which is why hsetroot is used instead
@@ -10,7 +12,8 @@ transmission-daemon
 
 # lightdm greeter as lock screen
 # only on keybind
-light-locker --no-lock-on-suspend --no-lock-on-lid &
+light-locker --no-late-locking --no-lock-on-suspend --no-lock-on-lid \
+             --no-idle-hint --lock-after-screensaver=0 &
 
 # Clipboard management:
 # parcellite for preserving clipboard contents on
