4ea17b15fa3d25cd3bb09a4fa1d29506fa7d0fe1
Tuure <tuure.piitulainen@gmail.com>
2024-05-03 22:16:05 +0300

    Don't install luap parser

--- .config/nvim/lua/conf/treesitter.lua
+++ .config/nvim/lua/conf/treesitter.lua
@@ -2,7 +2,10 @@ require'nvim-treesitter.configs'.setup({
     ensure_installed = "all",
     sync_install = false,
     auto_install = false,
-    ignore_install = { "comment" },
+    ignore_install = {
+        "comment", -- too complicated comment parsing
+        "luap"     -- too complicated regex parsing in lua
+    },
 
     highlight = {
         enable = true,
