fa0fb86da4d420afee736bbe66c2e6cfa957ca19
Tuure <tuure.piitulainen@gmail.com>
2024-06-24 22:52:30 +0300

    Center view while searching
    Hacky and experimental at this point, but may actually work
    
    Noticed that this "plugin" never actually considered the case of
    searching in visual modes, look into that later.

--- .config/nvim/plugin/search-improvements.lua
+++ .config/nvim/plugin/search-improvements.lua
@@ -1,12 +1,21 @@
 -- https://www.reddit.com/r/neovim/comments/zc720y/comment/iyvcdf0/?utm_source=share&utm_medium=web2x&context=3
 
 vim.on_key(function(char)
-    if vim.fn.mode() == "n" then
+    if vim.fn.mode() == "n" --[[ Todo: visual mode(s) not considered? ]] then
+
         local new_hlsearch = vim.tbl_contains(
             { "<CR>", "n", "N", "*", "#", "?", "/" },
             vim.fn.keytrans(char)
         )
+
         if vim.o.hlsearch ~= new_hlsearch then
+            -- Keep view centered while searching
+            if (new_hlsearch) then
+                vim.o.scrolloff = 999
+            else
+                vim.o.scrolloff = 0
+            end
+
             vim.o.hlsearch = new_hlsearch
         end
     end
