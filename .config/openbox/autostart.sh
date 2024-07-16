0ebabd2203cbe9e0db28b6e62d8c8904588d8fce
Tuure <tuure.piitulainen@gmail.com>
2024-07-16 12:15:06 +0300

    Remove unused startup command

--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -10,11 +10,6 @@ dunst &
 picom -b
 transmission-daemon
 
-# lightdm greeter as lock screen
-# only on keybind
-light-locker --no-late-locking --no-lock-on-suspend --no-lock-on-lid \
-             --no-idle-hint --lock-after-screensaver=0 &
-
 # Clipboard management:
 # parcellite for preserving clipboard contents on
 # application close, greenclip for history access
