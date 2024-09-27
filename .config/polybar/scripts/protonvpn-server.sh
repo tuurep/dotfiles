b69c0229f7a687340424959cbba5b3c82d4f7ae0
Tuure <tuure.piitulainen@gmail.com>
2024-09-27 22:49:41 +0300

    Add vpn disconnected label
    I like the idea of being able to click on the module, and Protonvpn-app
    autoconnects to the fastest server (in the app's settings..)
    
    Also why not, looks cool.

--- .config/polybar/scripts/protonvpn-server.sh
+++ .config/polybar/scripts/protonvpn-server.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-# Get connected ProtonVPN server name with lowercased country id
+# Must be same as polybar config's foreground-alt
+foreground_alt=#808080
 
-nmcli --colors no -f name con show --active \
-        | grep "ProtonVPN" \
-        | cut -d " " -f2 \
-        | tr "[:upper:]" "[:lower:]"
+if pgrep -x openvpn > /dev/null; then
+        # Get connected ProtonVPN server name with lowercased country id
+
+        server=$(nmcli --colors no -f name con show --active \
+                        | grep "ProtonVPN" \
+                        | cut -d " " -f2 \
+                        | tr "[:upper:]" "[:lower:]")
+
+        echo " $server"
+else
+        echo "%{F$foreground_alt} vpn%{F-}"
+fi
