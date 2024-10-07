0e84cfd26435b724e27995724227d805982ed01c
Tuure <tuure.piitulainen@gmail.com>
2024-10-07 17:06:07 +0300

    Clean up mullvad status script
    Noticed that it can sometimes output more than one lines
    
    Do `head -n1` early because we're only ever interested in the first line
    of `mullvad status`
    
    `printf` to `echo`: there should be a newline and the `printf` was from
    some earlier attempt where I wanted to extend the output later.

--- .config/polybar/scripts/mullvad-status.sh
+++ .config/polybar/scripts/mullvad-status.sh
@@ -3,11 +3,11 @@
 # Must be same as polybar config's foreground-alt
 foreground_alt=#808080
 
-status=$(mullvad status)
+status=$(mullvad status | head -n1)
 
 if [[ "$status" == "Connected"* ]]; then
         country=$(echo "$status" | cut -d " " -f3 | cut -d "-" -f1)
-        printf "%s" " $country"
+        echo " $country"
 
 elif [[ "$status" == "Connecting"* ]]; then
         echo " .."
