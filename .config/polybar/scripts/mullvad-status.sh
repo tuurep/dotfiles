fb2800c2704d2646503d95137ba4cdc0798f8956
Tuure <tuure.piitulainen@gmail.com>
2024-10-25 05:21:37 +0300

    Fix and improve mullvad-status script
    Fix:
    `mullvad status` output has changed drastically
    
    Improvement:
    Found the command `mullvad status listen`, which outputs on status
    change, so we can tail the script instead of interval
    
    Todo:
    The new `mullvad status` output looks bad on the click-left notification
    due to tab alignments on proportional font. Do some additional parsing.
    Removed the click-left for now, running `m status` on terminal is of
    course easy enough. Maybe add it back, maybe not.

--- .config/polybar/scripts/mullvad-status.sh
+++ .config/polybar/scripts/mullvad-status.sh
@@ -3,14 +3,15 @@
 # Must be same as polybar config's foreground-alt
 foreground_alt=#808080
 
-status=$(mullvad status 2> /dev/null | head -n1)
+mullvad status listen | while read -r line; do
+        country=$(mullvad relay get | sed -n "2p" | awk '{print $3}')
 
-if [[ "$status" == "Connected"* ]]; then
-        country=$(echo "$status" | cut -d " " -f3 | cut -d "-" -f1)
-        echo " $country"
-
-elif [[ "$status" == "Connecting"* ]]; then
-        echo " .."
-else
-        echo "%{F$foreground_alt} vpn%{F-}"
-fi
+        case $line in
+                "Connecting"|"Disconnecting")
+                        echo " ..";;
+                "Connected")
+                        echo " $country";;
+                "Disconnected")
+                        echo "%{F$foreground_alt} vpn%{F-}";;
+        esac
+done
