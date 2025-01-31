c8d97d5d8e373cb24d9260a32a909d9afdfe41d7
Tuure <tuure.piitulainen@gmail.com>
2025-01-31 07:38:32 +0200

    nvim: fix hlsearch problems
    Problems with:
    - vim-sneak
    - hlsearch not being turned on in Visual mode
    
    Messy solution

--- .config/nvim/plugin/search-improvements.lua
+++ .config/nvim/plugin/search-improvements.lua
@@ -1,7 +1,41 @@
--- https://www.reddit.com/r/neovim/comments/zc720y/comment/iyvcdf0/?utm_source=share&utm_medium=web2x&context=3
+-- 1. Always turn off hlsearch as soon as possible
+-- 2. Always center current search match (note: not sneak)
 
+local save_scrolloff = vim.o.scrolloff
+
+-- hlsearch: don't consider keypresses (of 'n' mostly) when using sneak's s/f/t
+local was_sneak_input = false
+
+vim.api.nvim_create_autocmd("User", {
+    pattern = "SneakEnter",
+    callback = function()
+        was_sneak_input = true
+    end
+})
+vim.api.nvim_create_autocmd("User", {
+    pattern = "SneakLeave",
+    callback = function()
+        was_sneak_input = false 
+    end
+})
+
+-- Idea:
+-- https://www.reddit.com/r/neovim/comments/zc720y/comment/iyvcdf0/?utm_source=share&utm_medium=web2x&context=3
 vim.on_key(function(char)
-    if vim.fn.mode() == "n" --[[ Todo: visual mode(s) not considered? ]] then
+
+    if was_sneak_input then
+        return
+    end
+
+    local mode = vim.api.nvim_get_mode().mode
+
+    -- Todo: Surely there's a better way to check "if I'm in Normal or Visual mode"?
+
+    -- Todo: During visual mode searches, hlsearch doesn't flip during the incsearch phase
+    --       *unless* I type characters like 'n' in the search itself
+    --       Why is this different with normal mode?
+
+    if mode == "n" or mode == "v" or mode == "V" or mode == "" then
 
         local new_hlsearch = vim.tbl_contains(
             { "<CR>", "n", "N", "*", "#", "?", "/" },
@@ -13,7 +47,7 @@ vim.on_key(function(char)
             if (new_hlsearch) then
                 vim.o.scrolloff = 999
             else
-                vim.o.scrolloff = 0
+                vim.o.scrolloff = save_scrolloff
             end
 
             vim.o.hlsearch = new_hlsearch
