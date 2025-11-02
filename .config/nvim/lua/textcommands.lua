97303bf09c36a317a85e65f9afdd4cce45359a11
Tuure Piitulainen <tuure.piitulainen@gmail.com>
2025-11-02 22:39:29 +0200

    nvim: rework count behavior for append_paste and delete_last_char
    On these, it's more useful to duplicate the amount of [action] done per
    line, than to extend the selection by [count] lines in normal mode. In
    visual selection, the same can be repeated on every selected line.

--- .config/nvim/lua/textcommands.lua
+++ .config/nvim/lua/textcommands.lua
@@ -1,17 +1,13 @@
 -- Collection of text-editing commands similar to builtin J:
 --
 --     In normal mode, operate on current line
---         (or [count] lines)
+--         (or [count] lines, where makes sense)
 --
 --     In visual mode, operate on selected lines
 --         (regardless of visual mode type)
 
 -- Todo:
 --     - Dot repeat doesn't work (what would that require?)
---     - Rethink count for:
---         - X:     delete last [count] characters in line(s)
---         - <C-p>: paste [count] times on line(s)
---         - (keep the others like they currently are)
 
 -- Helpers
 
@@ -20,8 +16,9 @@ local function is_visual_mode(mode)
     return mode == "v" or mode == "V" or mode == "\22"
 end
 
-local function get_line_range(mode)
-    local start_line, end_line
+local function get_line_range(mode, n_count_extend)
+    local end_line = vim.fn.getpos(".")[2]
+    local start_line = end_line - 1
 
     if is_visual_mode(mode) then
         local cursor_line = vim.fn.getpos(".")[2]
@@ -31,9 +28,8 @@ local function get_line_range(mode)
         start_line = math.min(cursor_line, other_line) - 1
         end_line = math.max(cursor_line, other_line)
 
-    elseif mode == "n" then
+    elseif n_count_extend then
         local count = vim.v.count1
-        start_line = vim.api.nvim_win_get_cursor(0)[1] - 1
         end_line = start_line + count
     end
 
@@ -50,49 +46,11 @@ end
 
 local M = {}
 
--- Append register to line as a oneliner
--- In visual mode, append to all lines in selection
-function M.append_paste(join_by_space)
-    local mode = vim.api.nvim_get_mode().mode
-    local start_line, end_line = get_line_range(mode)
-    local lines = vim.api.nvim_buf_get_lines(0, start_line, end_line, false)
-
-    local reg = vim.fn.getreg(vim.v.register)
-
-    if reg ~= "" then
-        local reg_joined = reg:gsub("\n$", ""):gsub("^%s*", "")
-                              :gsub("%s*$", ""):gsub("%s+", " ")
-
-        for i, line in ipairs(lines) do
-            -- First trim any existing trailing whitespace
-            -- Having one space there fresh out of insert mode is surprisingly common
-            line = line:gsub("%s*$", "")
-
-            local space
-            if join_by_space and line ~= "" then
-                space = " "
-            else
-                space = ""
-            end
-            lines[i] = line .. space .. reg_joined
-        end
-
-        vim.api.nvim_buf_set_lines(0, start_line, end_line, false, lines)
-    end
-
-    vim.fn.cursor(end_line, 0)
-    vim.cmd("normal! $")
-
-    if is_visual_mode(mode) then
-        esc()
-    end
-end
-
 -- Reduce all consecutive whitespace sections to 1 space
 -- Ignoring leading whitespace (indent) and stripping trailing whitespaces
 function M.squeeze_spaces()
     local mode = vim.api.nvim_get_mode().mode
-    local start_line, end_line = get_line_range(mode)
+    local start_line, end_line = get_line_range(mode, true)
     local lines = vim.api.nvim_buf_get_lines(0, start_line, end_line, false)
 
     for i, line in ipairs(lines) do
@@ -109,7 +67,7 @@ end
 -- For visual selection, delete lines and leave one blank line
 function M.wipe_line()
     local mode = vim.api.nvim_get_mode().mode
-    local start_line, end_line = get_line_range(mode)
+    local start_line, end_line = get_line_range(mode, true)
 
     local deleted_lines = vim.api.nvim_buf_get_lines(0, start_line, end_line, false)
     vim.fn.setreg(vim.v.register, deleted_lines)
@@ -122,17 +80,65 @@ function M.wipe_line()
     end
 end
 
--- Delete last character in line (or all selected lines)
-function M.delete_last_char()
+-- Append register to line as a oneliner
+-- In visual mode, append to all lines in selection
+-- Count to duplicate the append (per line in visual selection)
+function M.append_paste(join_by_space)
     local mode = vim.api.nvim_get_mode().mode
-    local start_line, end_line = get_line_range(mode)
+    local start_line, end_line = get_line_range(mode, false)
     local lines = vim.api.nvim_buf_get_lines(0, start_line, end_line, false)
 
-    for i, line in ipairs(lines) do
-        local char_count = vim.fn.strchars(line) -- account for multibyte chars
-        if char_count > 0 then
-            lines[i] = vim.fn.strcharpart(line, 0, char_count - 1)
+    local reg = vim.fn.getreg(vim.v.register)
+
+    if reg ~= "" then
+        local reg_joined = reg:gsub("\n$", ""):gsub("^%s*", "")
+                              :gsub("%s*$", ""):gsub("%s+", " ")
+
+        for i = 1, #lines do
+            local line = lines[i]
+            for _ = 1, vim.v.count1 do
+                -- First trim any existing trailing whitespace
+                -- Having one space there fresh out of insert mode is surprisingly common
+                line = line:gsub("%s*$", "")
+
+                local space
+                if join_by_space and line ~= "" then
+                    space = " "
+                else
+                    space = ""
+                end
+                line = line .. space .. reg_joined
+            end
+            lines[i] = line
         end
+
+        vim.api.nvim_buf_set_lines(0, start_line, end_line, false, lines)
+    end
+
+    vim.fn.cursor(end_line, 0)
+    vim.cmd("normal! $")
+
+    if is_visual_mode(mode) then
+        esc()
+    end
+end
+
+-- Delete last character in line (or all selected lines)
+-- With count, deletes [count] last characters in line(s)
+function M.delete_last_char()
+    local mode = vim.api.nvim_get_mode().mode
+    local start_line, end_line = get_line_range(mode, false)
+    local lines = vim.api.nvim_buf_get_lines(0, start_line, end_line, false)
+
+    for i = 1, #lines do
+        local line = lines[i]
+        for _ = 1, vim.v.count1 do
+            local char_count = vim.fn.strchars(line) -- account for multibyte chars
+            if char_count > 0 then
+                line = vim.fn.strcharpart(line, 0, char_count - 1)
+            end
+        end
+        lines[i] = line
     end
 
     vim.api.nvim_buf_set_lines(0, start_line, end_line, false, lines)
