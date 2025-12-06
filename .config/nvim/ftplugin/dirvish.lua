168d7af7af6563a1a0773852f7965c24e0bf7609
Tuure Piitulainen <tuure.piitulainen@gmail.com>
2025-12-06 13:38:53 +0200

    nvim: disable `.` bufferlocal map in dirvish

--- .config/nvim/ftplugin/dirvish.lua
+++ .config/nvim/ftplugin/dirvish.lua
@@ -1,10 +1,13 @@
-vim.keymap.set("n", "<C-q>", "<Plug>(dirvish_quit)", {buffer=0})
-vim.keymap.set({"n", "x"}, "<C-PageDown>", "<cr>", {remap=true, buffer=0})
-
--- Ain't nobody allowed to map my K
+-- Unwanted dirvish maps
+vim.cmd("silent! unmap <buffer> .")
 vim.cmd("silent! unmap <buffer> K")
 
-vim.keymap.set({"n", "x"}, "?", "<Plug>(dirvish_K)", {buffer=0})
+-- Remaps for above
+-- Todo: if . was useful, remap to something?
+vim.keymap.set({"n", "x"}, "?", "<Plug>(dirvish_K)", { buffer = 0 })
+
+vim.keymap.set("n", "<C-q>", "<Plug>(dirvish_quit)", { buffer = 0 })
+vim.keymap.set({"n", "x"}, "<C-PageDown>", "<cr>", { remap=true, buffer = 0 })
 
 local buf = vim.fn.expand("%")
 vim.opt_local.statusline = vim.fn.fnamemodify(buf, ":p:~") .. " %r%m"
