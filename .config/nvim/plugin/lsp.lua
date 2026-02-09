8315ccd4af1803f7c3c85e6214963a3482a00f2f
Tuure Piitulainen <tuure.piitulainen@gmail.com>
2026-02-09 17:48:02 +0200

    nvim: add rust LSP

--- .config/nvim/plugin/lsp.lua
+++ .config/nvim/plugin/lsp.lua
@@ -1,6 +1,15 @@
 vim.diagnostic.enable(false)
 
-vim.lsp.enable("basedpyright") -- AUR:    basedpyright
-vim.lsp.enable("clangd")       -- pacman: clang
-vim.lsp.enable("lua_ls")       -- pacman: lua_language_server
-vim.lsp.enable("ts_ls")        -- pacman: typescript_language_server
+-- List of configs in nvim-lspconfig:
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
+
+-- List of LSPs on Arch:
+-- https://wiki.archlinux.org/title/Language_Server_Protocol
+
+vim.lsp.enable({
+    "basedpyright",  -- AUR:    basedpyright
+    "clangd",        -- pacman: clang
+    "lua_ls",        -- pacman: lua_language_server
+    "rust_analyzer", -- pacman: rust-analyzer
+    "ts_ls"          -- pacman: typescript_language_server
+})
